76a195769753de9b24528a927e989a4e0ec5dce1
harshitsaraswat <harshitsaraswat2002@gmail.com>
2026-08-13 14:33:51 +0530

    Configure Aiven MySQL connection

--- configdatabase.py
+++ configdatabase.py
@@ -1,20 +1,28 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in .env")
+
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    connect_args={
+        "ssl_disabled": False,
+        "ssl_verify_cert": False,
+        "ssl_verify_identity": False,
+    },
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
